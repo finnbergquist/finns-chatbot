b436f86bd8fa4e2e38911be81593e87ced9f8861
finnbergquist <finnyb38@gmail.com>
2024-08-07 09:56:55 -0400

    Update events.tsx

--- components/stocks/events.tsx
+++ components/stocks/events.tsx
@@ -12,7 +12,7 @@ export function Events({ props: events }: { props: Event[] }) {
   return (
     <div className="-mt-2 flex w-full flex-col gap-2 py-4">
       <div className="flex flex-col gap-1 rounded-lg ">
-        <div style={{ width: '800px', transform: 'scale(0.75)', transformOrigin: 'top left' }}>
+        <div style={{ width: '950px', transform: 'scale(0.75)', transformOrigin: 'top left' }}>
           <GitHubCalendar username="finnbergquist" />
         </div>
       </div>
